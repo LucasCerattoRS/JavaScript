bcfe1e91c8ce4e7be8197198f55cb12ee2880db5
Lukas Ceratti Agnese <lukelucanolightknowledge@gmail.com>
2026-02-20 13:39:34 -0300

    Desenvolvimento Gerador Multiplo  De Numero Aleatorio

--- PraticaLogicaJs/sorteador-numeros/app.js
+++ PraticaLogicaJs/sorteador-numeros/app.js
@@ -3,15 +3,20 @@ function sortear() {
     quantidade = parseInt(document.getElementById("quantidade").value);
     de = parseInt(document.getElementById("de").value);
     ate = parseInt(document.getElementById("ate").value);
-}
-    if (de >= ate) {
-        alert("O valor 'de' deve ser menor que o valor 'até'.");
-        return;
-    }
+    // Utilizamos parseInt para converter os valores de string para números inteiros
     
-    if ((ate - de + 1) < quantidade) {
-        alert("O intervalo é muito pequeno para a quantidade de números solicitada.");
-        return;
+    let sorteados = [];
+    let numero;
+    // Cria uma váriavel para armazenar os números sorteados
+    // O laço for irá se repetir a quantidade de vezes definida pelo usuário, sorteando um número aleatório entre os valores "de" e "até" a cada iteração, e armazenando esses números no array "sorteados".
+    for (let i = 0; i < quantidade; i++) {
+        numero = obterNumeroAleatorio(de, ate);
+        sorteados.push(numero);
     }
 
-    let sorteados = [];
+    alert(sorteados);
+}
+
+    function obterNumeroAleatorio(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
