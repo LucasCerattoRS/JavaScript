62b4e7187f4c317c4a8ff8f21ca4bc305f02d2b7
Lukas Ceratti Agnese <lukelucanolightknowledge@gmail.com>
2026-03-15 21:54:05 -0300

    Adiciona botão de concluir tarefa no JVsDOM

--- JVsDOM/projeto_inicial/listaDeTarefas.js
+++ JVsDOM/projeto_inicial/listaDeTarefas.js
@@ -1,3 +1,6 @@
+// Envolver o código em uma função auto-invocada para evitar poluição do escopo global
+// Função auto-invocada para encapsular o código e evitar conflitos de variáveis no escopo global
+(() => {
 // Criar um novo item na lista de tarefas
 const criarTarefa = (evento) => {
         // Prevenir o comportamento padrão do formulário, que é recarregar a página
@@ -16,10 +19,11 @@ const criarTarefa = (evento) => {
         const conteudo = `<p class="content">${valor}</p>`
         // Definir o conteúdo HTML do elemento de lista com a nova tarefa
         tarefa.innerHTML = conteudo
+        tarefa.appendChild(BotaoConclui())
         // Adicionar o novo elemento de lista à lista de tarefas
         lista.appendChild(tarefa)
         // Limpar o campo de input após adicionar a tarefa
-        inputTarefa.value = " "
+        inputTarefa.value = ""
 }
 
 // Selecionar o botão e adicionar um evento de clique
@@ -27,3 +31,23 @@ const novaTarefa = document.querySelector('[data-form-button]')
 
 // Verificar se o elemento foi selecionado corretamente
 novaTarefa.addEventListener('click', criarTarefa)
+
+const BotaoConclui = () => {
+        const botaoConclui = document.createElement('button')
+        botaoConclui.classList.add('check-button')
+        botaoConclui.innerText = 'Concluir'
+                
+        botaoConclui.addEventListener('click' , concluirTarefa)
+
+        return botaoConclui
+}       
+
+const concluirTarefa = (evento) => {
+        // Obter o botão que foi clicado
+        const botaoConclui = evento.target
+        // Obter o elemento pai do botão, que é a tarefa (li) correspondente
+        const tarefaCompleta = botaoConclui.parentElement
+        // Adicionar a classe 'done' à tarefa para marcar como concluída
+        tarefaCompleta.classList.toggle('done')
+}
+})()
